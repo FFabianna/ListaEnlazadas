95c6bd9ffdce7bb4461fe36bba021cb8cb9914d5
Fabiana Valderruten Soler <1006557936@u.icesi.edu.co>
2022-04-04 09:59:51 -0500

    algo mas

--- src/model/ListaEnlazadaTail.java
+++ src/model/ListaEnlazadaTail.java
@@ -3,5 +3,24 @@ package model;
 public class ListaEnlazadaTail {
 	private Node head;
 	private Node tail;
+	
+	public void addFirst (Node node) {
+		if (head==null) {
+			head= node;
+			tail=node;
+		}else {
+			node.setNext(head);
+			head= node;
+		}
+	}
+	public void addLast (Node node) {
+		if (tail==null) {
+			head= node;
+			tail=node;
+		}else {
+			tail.setNext(head);
+			tail= node;
+		}
 
+}
 }
